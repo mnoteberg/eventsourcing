b27062fbcff8013be13662f0adb8aa81864a835a
Mikaela Nöteberg <mnoteberg@gmail.com>
2017-11-07 10:33:40 +0100

    replay

--- eventconsumer/src/main/java/se/ithuset/eventsourcing/api/BankController.java
+++ eventconsumer/src/main/java/se/ithuset/eventsourcing/api/BankController.java
@@ -1,5 +1,6 @@
 package se.ithuset.eventsourcing.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,7 @@ import se.ithuset.eventsourcing.model.Account;
 import se.ithuset.eventsourcing.model.Transaction;
 import se.ithuset.eventsourcing.service.AccountService;
 import se.ithuset.eventsourcing.service.TransactionService;
+import se.ithuset.eventsourcing.consumer.EventConsumer;
 
 import java.util.List;
 
@@ -16,9 +18,13 @@ public class BankController {
 
     private TransactionService transactionService;
 
-    public BankController(AccountService accountService, TransactionService transactionService) {
+    private EventConsumer eventConsumer;
+
+    @Autowired
+    public BankController(AccountService accountService, TransactionService transactionService, EventConsumer eventConsumer) {
         this.accountService = accountService;
         this.transactionService = transactionService;
+        this.eventConsumer = eventConsumer;
     }
 
     @GetMapping("/accounts/{accountId}")
@@ -34,5 +40,10 @@ public class BankController {
     @GetMapping("/accounts/{accountId}/transactions")
     public List<Transaction> getTransactions(@PathVariable("accountId") String accountId) {
         return transactionService.get(accountId);
+    }
+
+    @GetMapping("/accounts/replay/{offset}")
+    public void replay(@PathVariable("offset") int offset) {
+        eventConsumer.replay(offset);
     }
 }
--- eventconsumer/src/main/java/se/ithuset/eventsourcing/consumer/EventConsumer.java
+++ eventconsumer/src/main/java/se/ithuset/eventsourcing/consumer/EventConsumer.java
@@ -1,9 +1,11 @@
-package se.ithuset.eventsourcing.listener;
+package se.ithuset.eventsourcing.consumer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.TopicPartition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.listener.ConsumerSeekAware;
 import org.springframework.stereotype.Component;
 import se.bank.event.AccountClosed;
 import se.bank.event.AccountCreated;
@@ -17,14 +19,17 @@ import java.io.IOException;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.Map;
 
 @Component
-public class EventConsumer {
+public class EventConsumer implements ConsumerSeekAware {
     private AccountService accountService;
 
     private TransactionService transactionService;
 
     private ObjectMapper objectMapper;
+
+    private ConsumerSeekCallback callback;
 
     @Autowired
     public EventConsumer(AccountService accountService, TransactionService transactionService, ObjectMapper objectMapper) {
@@ -60,5 +65,25 @@ public class EventConsumer {
                 accountService.calculateInterest(timestamp);
                 break;
         }
+    }
+
+    @Override
+    public void registerSeekCallback(ConsumerSeekCallback callback) {
+        System.out.println("EventConsumer.registerSeekCallback");
+        this.callback = callback;
+    }
+
+    @Override
+    public void onPartitionsAssigned(Map<TopicPartition, Long> assignments, ConsumerSeekCallback callback) {
+        System.out.println("EventConsumer.onPartitionsAssigned");
+    }
+
+    @Override
+    public void onIdleContainer(Map<TopicPartition, Long> assignments, ConsumerSeekCallback callback) {
+        System.out.println("EventConsumer.onIdleContainer");
+    }
+
+    public void replay(int offset) {
+        callback.seek("lab", 0, offset);
     }
 }
